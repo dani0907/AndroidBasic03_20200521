90e30c1c15a70e221c1da50af72f82ae63962937
dy696786 <dy696786@gmail.com>
2020-05-21 15:59:22 +0900

    닉네임 변경 프로세스  - 다른 액티비티를 통해 입력값 받아서 반영

--- app/src/main/java/com/example/androidbasic03_20200521/EditUserNickNameActivity.java
+++ app/src/main/java/com/example/androidbasic03_20200521/EditUserNickNameActivity.java
@@ -1,21 +1,42 @@
 package com.example.androidbasic03_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic03_20200521.databinding.ActivityEditUserNickNameBinding;
 
 public class EditUserNickNameActivity extends BaseActivity {
+
+    ActivityEditUserNickNameBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_nick_name);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_user_nick_name);
         setupEvents();
         setValues();
     }
 
     @Override
     public void setupEvents() {
+
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nickName = binding.nickNameEdt.getText().toString();
+
+                Intent myIntent = new Intent();
+                myIntent.putExtra("editNickName",nickName);
+
+                setResult(RESULT_OK, myIntent);
+
+                finish();
+            }
+        });
 
     }
 
