53cfd517ab44ad1342b0646f0abb4341eaf0fd8f
dy696786 <dy696786@gmail.com>
2020-05-21 16:07:20 +0900

    전화번호 수정 화면 그리기

---
+++ app/src/main/java/com/example/androidbasic03_20200521/EditUserPhoneActivity.java
@@ -0,0 +1,25 @@
+package com.example.androidbasic03_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserPhoneActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_phone);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
